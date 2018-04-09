7e200564248f851dba221e44a59aeb14e71a93fd
Petra Thaipanich <Petra.Th@BDMS.CO.TH>
2018-04-09 16:34:00 +0700

    - Test

--- bot.php
+++ bot.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
  
 // include composer autoload
 // require_once '../vendor/autoload.php';
-require_once '../app/vendor/autoload.php';
+require_once '/vendor/autoload.php';
  
 // การตั้งเกี่ยวกับ bot
 require_once 'bot_settings.php';
@@ -58,79 +58,78 @@ $content = file_get_contents('php://input');
  
 // แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
 $events = json_decode($content, true);
+
 if(!is_null($events)){
     // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
     $replyToken = $events['events'][0]['replyToken'];
     $typeMessage = $events['events'][0]['message']['type'];
-    $userMessage = $events['events'][0]['message']['text'];
-    $userMessage = strtolower($userMessage);
+    $userMessage = strtolower($events['events'][0]['message']['text']);
+   
     switch ($typeMessage){
         case 'text':
             switch ($userMessage) {
-                case "t":
-                    $textReplyMessage = "Bot ตอบกลับคุณเป็นข้อความ";
-                    $replyData = new TextMessageBuilder($textReplyMessage);
-                    break;
                 case "i":
-                    $picFullSize = 'https://www.mywebsite.com/imgsrc/photos/f/simpleflower';
-                    $picThumbnail = 'https://www.mywebsite.com/imgsrc/photos/f/simpleflower/240';
-                    $replyData = new ImageMessageBuilder($picFullSize,$picThumbnail);
+                    $arr_replyData = array();
+
+                    $textReplyMessage   = "Bot ตอบกลับคุณเป็นข้อความ";
+                    $arr_replyData[]    = new TextMessageBuilder($textReplyMessage);
+
+                    $picFullSize        = 'https://www.google.co.th/imgres?imgurl=http://s3-ap-southeast-1.amazonaws.com/wpimage.shopspotapp.com/wp-content/uploads/2017/08/08120504/20480018_1123379674464780_8404745370706867972_n.jpg&imgrefurl=http://article.shopspotapp.com/ss-article/world-cat-day/&h=960&w=960&tbnid=ZH-_Yp7vp-fjFM:&tbnh=186&tbnw=186&usg=__LMSN7AXR_kTlJ8PmnUQtMkBeJ_w%3D&vet=10ahUKEwj-9qXL7azaAhWqHJoKHTehCf0Q_B0IgQIwDQ..i&docid=kgnsD3LLHtUTGM&itg=1&sa=X&ved=0ahUKEwj-9qXL7azaAhWqHJoKHTehCf0Q_B0IgQIwDQ';
+                    $picThumbnail       = 'https://www.google.co.th/imgres?imgurl=http://s3-ap-southeast-1.amazonaws.com/wpimage.shopspotapp.com/wp-content/uploads/2017/08/08120504/20480018_1123379674464780_8404745370706867972_n.jpg&imgrefurl=http://article.shopspotapp.com/ss-article/world-cat-day/&h=960&w=960&tbnid=ZH-_Yp7vp-fjFM:&tbnh=186&tbnw=186&usg=__LMSN7AXR_kTlJ8PmnUQtMkBeJ_w%3D&vet=10ahUKEwj-9qXL7azaAhWqHJoKHTehCf0Q_B0IgQIwDQ..i&docid=kgnsD3LLHtUTGM&itg=1&sa=X&ved=0ahUKEwj-9qXL7azaAhWqHJoKHTehCf0Q_B0IgQIwDQ';
+                    $arr_replyData[]    = new ImageMessageBuilder($picFullSize,$picThumbnail);
+
+                    $placeName          = "ที่ตั้งร้าน";
+                    $placeAddress       = "แขวง พลับพลา เขต วังทองหลาง กรุงเทพมหานคร ประเทศไทย";
+                    $latitude           = 13.780401863217657;
+                    $longitude          = 100.61141967773438;
+                    $arr_replyData[]    = new LocationMessageBuilder($placeName, $placeAddress, $latitude ,$longitude);  
+                    
+                    $multiMessage       = new MultiMessageBuilder;
+                    foreach($arr_replyData as $arr_Reply){
+                            $multiMessage->add($arr_Reply);
+                    }
+                    $replyData = $multiMessage;                                     
                     break;
-                case "v":
-                    $picThumbnail = 'https://www.mywebsite.com/imgsrc/photos/f/sampleimage/240';
-                    $videoUrl = "https://www.mywebsite.com/simplevideo.mp4";                
-                    $replyData = new VideoMessageBuilder($videoUrl,$picThumbnail);
-                    break;
-                case "a":
-                    $audioUrl = "https://www.mywebsite.com/simpleaudio.mp3";
-                    $replyData = new AudioMessageBuilder($audioUrl,27000);
-                    break;
-                case "l":
-                    $placeName = "ที่ตั้งร้าน";
-                    $placeAddress = "แขวง พลับพลา เขต วังทองหลาง กรุงเทพมหานคร ประเทศไทย";
-                    $latitude = 13.780401863217657;
-                    $longitude = 100.61141967773438;
-                    $replyData = new LocationMessageBuilder($placeName, $placeAddress, $latitude ,$longitude);              
-                    break;
+                    
                 case "s":
                     $stickerID = 22;
                     $packageID = 2;
                     $replyData = new StickerMessageBuilder($packageID,$stickerID);
                     break;      
-                case "im":
-                    $imageMapUrl = 'https://www.mywebsite.com/imgsrc/photos/w/sampleimagemap';
-                    $replyData = new ImagemapMessageBuilder(
-                        $imageMapUrl,
-                        'This is Title',
-                        new BaseSizeBuilder(699,1040),
-                        array(
-                            new ImagemapMessageActionBuilder(
-                                'test image map',
-                                new AreaBuilder(0,0,520,699)
-                                ),
-                            new ImagemapUriActionBuilder(
-                                'http://www.ninenik.com',
-                                new AreaBuilder(520,0,520,699)
-                                )
-                        )); 
-                    break;          
-                case "tm":
-                    $replyData = new TemplateMessageBuilder('Confirm Template',
-                        new ConfirmTemplateBuilder(
-                                'Confirm template builder',
-                                array(
-                                    new MessageTemplateActionBuilder(
-                                        'Yes',
-                                        'Text Yes'
-                                    ),
-                                    new MessageTemplateActionBuilder(
-                                        'No',
-                                        'Text NO'
-                                    )
-                                )
-                        )
-                    );
-                    break;                                                                                                                          
+                // case "im":
+                //     $imageMapUrl = 'https://www.mywebsite.com/imgsrc/photos/w/sampleimagemap';
+                //     $replyData = new ImagemapMessageBuilder(
+                //         $imageMapUrl,
+                //         'This is Title',
+                //         new BaseSizeBuilder(699,1040),
+                //         array(
+                //             new ImagemapMessageActionBuilder(
+                //                 'test image map',
+                //                 new AreaBuilder(0,0,520,699)
+                //                 ),
+                //             new ImagemapUriActionBuilder(
+                //                 'http://www.ninenik.com',
+                //                 new AreaBuilder(520,0,520,699)
+                //                 )
+                //         )); 
+                //     break;          
+                // case "tm":
+                //     $replyData = new TemplateMessageBuilder('Confirm Template',
+                //         new ConfirmTemplateBuilder(
+                //                 'Confirm template builder',
+                //                 array(
+                //                     new MessageTemplateActionBuilder(
+                //                         'Yes',
+                //                         'Text Yes'
+                //                     ),
+                //                     new MessageTemplateActionBuilder(
+                //                         'No',
+                //                         'Text NO'
+                //                     )
+                //                 )
+                //         )
+                //     );
+                //     break;                                                                                                                          
                 default:
                     $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
                     $replyData = new TextMessageBuilder($textReplyMessage);         
@@ -142,15 +141,16 @@ if(!is_null($events)){
             $replyData = new TextMessageBuilder($textReplyMessage);         
             break;  
     }
-}
 //l ส่วนของคำสั่งตอบกลับข้อความ
-$response = $bot->replyMessage($replyToken,$replyData);
+$response = $bot->replyMessage($replyToken , $replyData);
  
-if ($response->isSucceeded()) {
-    echo 'Succeeded!';
-    return;
-}
- 
+    if ($response->isSucceeded()) {
+        echo 'Succeeded!';
+        return;
+    }
+
 // Failed
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+ 
+}
 ?>
